83b53cb7b5e78a60c0fd89068e382183b05e6c4b
cb-manick <manick.s@chargebee.com>
2022-01-31 10:46:42 +0530

    Fixed Internal Provider - ALl Errors

--- internal-provider-reference/src/main/java/io/hubble/service/InvoiceNumberService.java
+++
@@ -1,9 +0,0 @@
-package io.hubble.service;
-
-import io.hubble.model.GenerateInvoiceNumberRequest;
-import io.hubble.model.GenerateInvoiceNumberResponse;
-
-public interface InvoiceNumberService {
-
-  GenerateInvoiceNumberResponse generateInvoiceNumber(GenerateInvoiceNumberRequest request);
-}

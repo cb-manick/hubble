e1ecb37c30d9759aabae012ef0679a10e6374a90
cb-manick <manick.s@chargebee.com>
2022-02-01 12:23:12 +0530

    Core Json

--- internal-provider-pdfgeneration/src/main/java/io/hubble/pdf/service/PDFGenerationServiceImpl.java
+++ internal-provider-pdfgeneration/src/main/java/io/hubble/pdf/service/PDFGenerationServiceImpl.java
@@ -4,6 +4,8 @@ import io.hubble.pdf.model.GeneratePdf;
 import io.hubble.pdf.model.PdfGenerated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,9 @@ import java.util.function.Function;
 @Service
 public class PDFGenerationServiceImpl implements PDFGenerationService {
   private static final Logger logger = LoggerFactory.getLogger(PDFGenerationServiceImpl.class);
+
+  @Autowired
+  private StreamBridge streamBridge;
 
   @Override
   public PdfGenerated generatePdf(GeneratePdf generatePdf) {

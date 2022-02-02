d07f0d180d50e5f6c3ff126d575fdf42fceec1ac
cb-manick <manick.s@chargebee.com>
2022-02-02 12:33:04 +0530

    Added Types.go main.go

--- hubble-connect/main.go
+++ hubble-connect/main.go
@@ -22,4 +22,5 @@ func main() {
 	event.SetDataContentType("")
 	fmt.Println(event)
 	fmt.Println(hubbleTypes.RegisteredCapabilitiesData)
+	fmt.Println(hubbleTypes.RegisteredProviderData)
 }
--- hubble-connect/pkg/types/Types.go
+++ hubble-connect/pkg/types/Types.go
@@ -21,13 +21,20 @@ type Ownership struct {
 
 var RegisteredCapabilitiesData = []RegisteredCapability{
 	RegisteredCapability{
-		ID:  "0001",
-		Urn: "hubble.io/invoice/InvoiceNumberGeneration",
+		ID:          "0001",
+		Urn:         "hubble.io/invoice/InvoiceNumberGeneration",
 		Description: "Invoice Number Generation",
 	},
 	RegisteredCapability{
-		ID:  "0002",
-		Urn: "hubble.io/invoice/PDFGeneration",
+		ID:          "0002",
+		Urn:         "hubble.io/invoice/PDFGeneration",
 		Description: "Invoice PDF Generation",
 	},
+}
+
+var RegisteredProviderData = []RegisteredProvider{
+	{"PROVIDER-001", "hubble.io/providers/providers/provider001", "0001", "Registered Provider Data"},
+	{"PROVIDER-001", "hubble.io/providers/providers/provider001", "0002", "Registered Provider Data"},
+	{"PROVIDER-002", "hubble.io/providers/providers/provider002", "0001", "Registered Provider Data"},
+	{"PROVIDER-002", "hubble.io/providers/providers/provider002", "0002", "Registered Provider Data"},
 }

fb1dff9e198d8fe3dd6730157a784ce991d034a2
cb-manick <manick.s@chargebee.com>
2022-02-22 12:31:23 +0530

    Add Calling Context

--- hubble-sdk/src/main/java/com/chargebee/hubble/model/CallingContext.java
+++ hubble-sdk/src/main/java/com/chargebee/hubble/model/CallingContext.java
@@ -7,6 +7,22 @@ public class CallingContext {
     private String capabilityId;
     private String operationName;
 
+    public void setEntityId(String entityId) {
+        this.entityId = entityId;
+    }
+
+    public void setMerchantId(String merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public void setCapabilityId(String capabilityId) {
+        this.capabilityId = capabilityId;
+    }
+
+    public void setOperationName(String operationName) {
+        this.operationName = operationName;
+    }
+
     public CallingContext(String entityId, String merchantId, String capabilityId, String operationName){
         this.entityId = entityId;
         this.merchantId = merchantId;
@@ -18,8 +34,6 @@ public class CallingContext {
         return entityId;
     }
 
-    public void setEntityId(String entityId) {
-        this.entityId = entityId;
-    }
+
 
 }

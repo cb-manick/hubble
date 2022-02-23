f2baaac85cf406a9f304bf88f60b1c93f76967ca
cb-manick <manick.s@chargebee.com>
2022-02-23 18:21:37 +0530

    Read Response

---
+++ hubble-sdk/src/main/java/com/chargebee/hubble/runtime/ReadResponse.java
@@ -0,0 +1,9 @@
+package com.chargebee.hubble.runtime;
+
+import java.util.Map;
+
+public class ReadResponse {
+    private byte[] data;
+    private Map<String, String> metadata;
+    private String contentType;
+}

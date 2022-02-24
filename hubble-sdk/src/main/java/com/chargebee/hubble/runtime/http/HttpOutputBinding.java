6dae2cc3df20f5d4ba4d08f973b7dbb171039a90
cb-manick <manick.s@chargebee.com>
2022-02-24 12:04:13 +0530

    Add Http Output binding

--- hubble-sdk/src/main/java/com/chargebee/hubble/runtime/http/HttpOutputBinding.java
+++ hubble-sdk/src/main/java/com/chargebee/hubble/runtime/http/HttpOutputBinding.java
@@ -1,34 +1,38 @@
 package com.chargebee.hubble.runtime.http;
 
 import com.chargebee.hubble.runtime.*;
+import com.chargebee.hubble.runtime.http.utils.HttpMetaData;
+import com.chargebee.hubble.runtime.http.utils.HttpMetaDataParser;
 
 import java.util.List;
 import java.util.Map;
 
 public class HttpOutputBinding implements OutputBinding {
-    private Map<String,String> metadata;
-    private static final String FORMATTED_PATH = "PATH";
-    private static final String FORMATTED_QUERY = "QUERY";
-    private static final String HOST = "HOST";
+  private Map<String, String> metadata;
+  private static final String FORMATTED_PATH = "PATH";
+  private static final String FORMATTED_QUERY = "QUERY";
+  private static final String HOST = "HOST";
 
-    public HttpOutputBinding() {
+  public HttpOutputBinding() {}
 
-    }
-    @Override
-    public void init(Map<String, String> metadata) {
-        this.metadata = metadata;
-    }
+  @Override
+  public void init(Map<String, String> metadata) {
+    this.metadata = metadata;
+  }
 
-    @Override
-    public InvokeResponse invoke(InvokeRequest invokeRequest) {
-        byte[] payload = invokeRequest.getData();
+  @Override
+  public InvokeResponse invoke(InvokeRequest invokeRequest) {
+    byte[] payload = invokeRequest.getData();
+    HttpMetaData metaData = HttpMetaDataParser.parse(invokeRequest.getMetadata());
+    return invoke(payload, metaData, invokeRequest.getOperation());
+  }
 
-        return null;
-    }
+  public InvokeResponse invoke(byte[] payload, HttpMetaData metaData, String operation) {
+    return null;
+  }
 
-    @Override
-    public List<String> getOperations() {
-        return null;
-    }
-
+  @Override
+  public List<String> getOperations() {
+    return null;
+  }
 }
--- hubble-sdk/src/main/java/com/chargebee/hubble/runtime/http/utils/HttpMetaData.java
+++ hubble-sdk/src/main/java/com/chargebee/hubble/runtime/http/utils/HttpMetaData.java
@@ -5,25 +5,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpMetaData {
-    public final Map<String,String> query;
-    public final Map<String,String> path;
-    public final Map<String,String> header;
+  public final Map<String, String> query;
+  public final Map<String, String> path;
+  public final Map<String, String> header;
 
-    public HttpMetaData(Map<String,String> query, Map<String,String> path, Map<String,String> header){
-        this.query = Collections.unmodifiableMap(query);
-        this.path = Collections.unmodifiableMap(path);
-        this.header = Collections.unmodifiableMap(header);
-    }
+  public HttpMetaData(
+      Map<String, String> query, Map<String, String> path, Map<String, String> header) {
+    this.query = Collections.unmodifiableMap(query);
+    this.path = Collections.unmodifiableMap(path);
+    this.header = Collections.unmodifiableMap(header);
+  }
 
-    public Map<String, String> getQuery() {
-        return query;
-    }
+  public Map<String, String> getQuery() {
+    return query;
+  }
 
-    public Map<String, String> getPath() {
-        return path;
-    }
+  public Map<String, String> getPath() {
+    return path;
+  }
 
-    public Map<String, String> getHeader() {
-        return header;
-    }
+  public Map<String, String> getHeader() {
+    return header;
+  }
 }

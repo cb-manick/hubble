653a6d50d4c2ca1711ba9db158e32401ef3960a2
cb-manick <manick.s@chargebee.com>
2022-02-24 11:53:01 +0530

    Add Http Meta Data Parser

--- hubble-sdk/src/main/java/com/chargebee/hubble/runtime/http/utils/HttpMetaDataParser.java
+++ hubble-sdk/src/main/java/com/chargebee/hubble/runtime/http/utils/HttpMetaDataParser.java
@@ -5,26 +5,26 @@ import java.util.Map;
 import java.util.Set;
 
 public class HttpMetaDataParser {
-    private static final String PATH_PREFIX="p_";
-    private static final String QUERY_PREFIX = "q_";
-    private static final String HEADER_PREFIX = "h_";
-    private static final int PARAM_BEGINS_WITH_INDEX = PATH_PREFIX.length();
+  public static final String PATH_PREFIX = "p_";
+  public static final String QUERY_PREFIX = "q_";
+  public static final String HEADER_PREFIX = "h_";
+  private static final int PARAM_BEGINS_WITH_INDEX = PATH_PREFIX.length();
 
-    public static HttpMetaData parse(Map<String,String> metaData) {
-       Map<String,String> path = new HashMap<>();
-       Map<String,String> query = new HashMap<>();
-       Map<String,String> header = new HashMap<>();
-        final Set<String> keys = metaData.keySet();
-        for (String key:keys) {
-           if (key.startsWith(PATH_PREFIX)) {
-               path.put(key.substring(PARAM_BEGINS_WITH_INDEX),metaData.get(key));
-           } else if (key.startsWith(QUERY_PREFIX)) {
-               query.put(key.substring(PARAM_BEGINS_WITH_INDEX),metaData.get(key));
-            } else if (key.startsWith(HEADER_PREFIX)) {
-               header.put(key.substring(PARAM_BEGINS_WITH_INDEX),metaData.get(key));
-           }
-       }
-
-       return new HttpMetaData(query,path,header);
+  public static HttpMetaData parse(Map<String, String> metaData) {
+    Map<String, String> path = new HashMap<>();
+    Map<String, String> query = new HashMap<>();
+    Map<String, String> header = new HashMap<>();
+    final Set<String> keys = metaData.keySet();
+    for (String key : keys) {
+      if (key.startsWith(PATH_PREFIX)) {
+        path.put(key.substring(PARAM_BEGINS_WITH_INDEX), metaData.get(key));
+      } else if (key.startsWith(QUERY_PREFIX)) {
+        query.put(key.substring(PARAM_BEGINS_WITH_INDEX), metaData.get(key));
+      } else if (key.startsWith(HEADER_PREFIX)) {
+        header.put(key.substring(PARAM_BEGINS_WITH_INDEX), metaData.get(key));
+      }
     }
+
+    return new HttpMetaData(query, path, header);
+  }
 }
--- hubble-sdk/src/test/java/com/chargebee/hubble/runtime/http/utils/HttpMetaDataParserTest.java
+++ hubble-sdk/src/test/java/com/chargebee/hubble/runtime/http/utils/HttpMetaDataParserTest.java
@@ -1,12 +1,25 @@
 package com.chargebee.hubble.runtime.http.utils;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HttpMetaDataParserTest {
+public class HttpMetaDataParserTest {
 
-    @Test
-    public void
-
+  @Test
+  public void testHttpMetaData() {
+    Map<String, String> httpMetaData = new HashMap<>();
+    httpMetaData.put("h_ID", "Auth-X");
+    httpMetaData.put("p_ID", "Auth-X");
+    httpMetaData.put("q_ID", "Auth-X");
+    httpMetaData.put("D", "Auth-X");
+    HttpMetaData parsedMetaData = HttpMetaDataParser.parse(httpMetaData);
+    Assertions.assertTrue(parsedMetaData.getPath().size() == 1);
+    Assertions.assertTrue(parsedMetaData.getQuery().size() == 1);
+    Assertions.assertTrue(parsedMetaData.getHeader().size() == 1);
+  }
 }

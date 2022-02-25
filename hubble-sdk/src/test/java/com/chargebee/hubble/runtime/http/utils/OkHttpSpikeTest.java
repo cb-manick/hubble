feb00fd5481450f0c64b8a700c34b732f68a1ba0
cb-manick <manick.s@chargebee.com>
2022-02-25 11:49:51 +0530

    OkHttpSpikeTest-Get

---
+++ hubble-sdk/src/test/java/com/chargebee/hubble/runtime/http/utils/OkHttpSpikeTest.java
@@ -0,0 +1,32 @@
+package com.chargebee.hubble.runtime.http.utils;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Response;
+import org.junit.Test;
+import com.squareup.okhttp.Request;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import java.io.IOException;
+
+public class OkHttpSpikeTest {
+  private final  String BASE_URL="http://localhost:8082/";
+  private final  OkHttpClient client = new OkHttpClient();
+
+
+    @Test
+    public void getTest(){
+        Request request = new Request.Builder()
+                .url(BASE_URL + "/get")
+                .build();
+        Call call = client.newCall(request);
+        Response response = null;
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assertions.assertEquals(response.code(), 200);
+    }
+}
